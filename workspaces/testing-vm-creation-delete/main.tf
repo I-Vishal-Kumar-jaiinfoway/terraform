f00ce9cade2473705ee81c813cf7f162f4bef287
I-Vishal-Kumar-jaiinfoway <vishal.kumar2@jaiinfoway.com>
2026-04-02 08:54:20 +0530

    Delete e2-micro VM 'testing-vm-creation' in us-central1-a via Terraform

---
+++ workspaces/testing-vm-creation-delete/main.tf
@@ -0,0 +1,7 @@
+provider "google" {
+  project = "aerial-chimera-321316"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+# No resources defined here; removal of this block will destroy the VM via Terraform.
