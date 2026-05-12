d58c500c850718909a9dc17dbe3037b01fd30cd2
I-Vishal-Kumar-jaiinfoway <vishal.kumar2@jaiinfoway.com>
2026-05-13 01:21:34 +0530

    Delete VM 'dev-test-vm' in us-central1-a via Terraform

--- workspaces/vm-delete-dev-test-vm/main.tf
+++ workspaces/vm-delete-dev-test-vm/main.tf
@@ -4,4 +4,35 @@ provider "google" {
   zone    = "us-central1-a"
 }
 
-# No resources defined: this signals terraform to delete the VM previously managed in this workspace
+resource "google_compute_instance" "dev_test_vm" {
+  name         = "dev-test-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  count        = 0
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2204-lts"
+      size  = 10
+    }
+    auto_delete = true
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["dev", "test"]
+  labels = {
+    env                        = "dev"
+    goog-terraform-provisioned = "true"
+    managed_by                 = "intellibooks-agent"
+  }
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+  lifecycle {
+    prevent_destroy = false
+  }
+}
